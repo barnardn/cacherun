c45983b989cefa005d36107d2de376bce3687c0b
Norm Barnard <norm@normbarnard.com>
2026-01-03 13:51:06 -0500

    rename entry point and own help and version arguments

--- Sources/cacherun/cacherun.swift
+++ Sources/cacherun/cacherun.swift
@@ -12,28 +12,40 @@ import CacheExecutor
 
 @main
 struct CacheRun: ParsableCommand {
+    static let toolVersion = "2.0.0"
+
     @Option(name: .shortAndLong, help: "cached output expiration time in seconds")
     var cacheTime: Int = 60
     
     @Flag(name: .shortAndLong, help: "display information about the commands currently cached")
     var listCaches = false
 
-    @Flag(name: .shortAndLong)
-    var help: Bool = false
-    
     @Option(name: .shortAndLong, help: "deletes all the files assocated to the command identified by <cacheid>")
     var deleteCache: String?
     
     @Option(name: .shortAndLong, help: "resets the cache files for the command identified by <cacheid>, forcing the command to be executed the next time it's run")
     var resetCache: String?
-    
+
+    // implement our own version and help due to `userCommand` argument
+    @Flag(name: .long, help: "Show version and exit")
+    var version = false
+
+    @Flag(name: [.customShort("H"), .customLong("show-help")], help: "Show help and exit. NOTE: ignore standard `--help' flag")
+    var showHelp = false
+
     @Argument(parsing: .captureForPassthrough, help: "command <flags> <args>")
     var userCommand: [String] = []
     
     mutating func run() throws {
-            
-        guard !help else { throw CleanExit.helpRequest(self) }
-        
+        // NOTE: we need to handle these args separately as `.captureForPassthrough` swallows the standard --version and --help flags
+        guard !version else {
+            print(Self.toolVersion)
+            throw CleanExit.message("")
+        }
+        guard !showHelp else {
+            throw CleanExit.helpRequest(self)
+        }
+
         if listCaches {
             OutputCachingExecutor.CacheManagement.showCachedCommands()
         } else if let deleteCache = deleteCache {
@@ -63,3 +75,4 @@ struct CacheRun: ParsableCommand {
         }
     }
 }
+
